cf46edd8a77d87a669ead3614d49abe1af89fc8a
伯虎 <2413081441@qq.com>
2023-04-04 09:37:08 +0800

    去掉tailwind警告

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx,vue}'],
+  darkMode: 'media', // false or 'media' or 'class'
   theme: {
     extend: {},
   },
