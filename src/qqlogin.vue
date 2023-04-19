7e921941f2e8d7972ec86d8601014197aa5bb2c1
skytangleisky <2413081441@qq.com>
2023-04-19 16:35:06 +0800

    解决窗口会自动关闭的问题

--- src/qqlogin.vue
+++ src/qqlogin.vue
@@ -7,11 +7,17 @@
     var pair = vars[i].split("=");
     params[pair[0]] = decodeURIComponent(pair[1])
   }
-  axios.get('/qqAuth/oauth2.0/me',{params:{access_token:params.access_token}}).then(res=>{
-    let me = JSON.parse(res.data.substring("callback( ".length,res.data.length-" );\n".length))
-    axios.get('/qqAuth/user/get_user_info',{params:{access_token:'71AA1705D35755D2B41DE579CED4F5BD',oauth_consumer_key:me.client_id,openid:me.openid}}).then(res=>{
-      window.opener&&window.opener.auth&&window.opener.auth(res.data)
-      window.close()
+  if(params.access_token){
+    axios.get('/qqAuth/oauth2.0/me',{params:{access_token:params.access_token}}).then(res=>{
+      let me = JSON.parse(res.data.substring("callback( ".length,res.data.length-" );\n".length))
+      if(me.client_id&&me.openid){
+        axios.get('/qqAuth/user/get_user_info',{params:{access_token:params.access_token,oauth_consumer_key:me.client_id,openid:me.openid}}).then(res=>{
+          if(res.data.ret!=-1){
+            window.opener&&window.opener.auth&&window.opener.auth(res.data)
+            window.close()
+          }
+        })
+      }
     })
-  })
+  }
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import container from '../container.vue'
-import qqLogin from '../qqlogin.vue'
+// import qqLogin from '../qqlogin.vue'
 export const constantRoutes = [{
   path: '/',
   component: container
 },{
   path: '/qqlogin',
-  component: qqLogin
+  component: () => import('../qqlogin.vue') //最好是异步加载
 }]
 const router = createRouter({
   history: createWebHistory(),
