b2bf634935bebdc0b30a85324a9a4e73c6e60164
伯虎 <2413081441@qq.com>
2023-04-25 21:08:44 +0800

    导航栏支持亮暗主题

--- src/myComponents/menu/index.vue
+++ src/myComponents/menu/index.vue
@@ -2,7 +2,7 @@
   <ol class="menu flex items-center content-center flex-row m-0"><!--导航栏-->
     <li tabindex="-1" v-for="(item,index) in list" :key="index">
       {{ item.name }}
-      <ol class="absolute flex-col p-0 left-0 top-100%" v-if="item.children"><!--一级菜单-->
+      <ol class="absolute flex-col left-0 p-0 top-100%" v-if="item.children" :style="'left:' + (item.left?'-1em':'0')"><!--一级菜单-->
         <submenu :item.sync="item"></submenu>
       </ol>
     </li>
@@ -29,7 +29,7 @@
     list-style: none;
     position: relative;
     display: none;
-    background-color: rgb(42, 42, 40);
+    background: lightgrey;
     li{
       position: relative;
       display: inline-flex;
@@ -43,26 +43,52 @@
       display: flex;
     }
   }
+  .dark ol{
+    background-color: rgb(42, 42, 40);
+  }
   .menu{
     background-color: transparent;
     &>li:hover{
-      background-color: rgb(53, 54, 51);
+      background-color: lightgrey;
       border-radius: 4px;
       li{
         align-items: center;
         justify-content: space-between;
         &:hover{
-          background-color: rgb(62, 110, 197);
+          background-color: rgba(62, 110, 197, .5);
           border-radius: 4px;
         }
         &:has(ol:hover){
-          background-color: rgb(64, 64, 62);
+          background-color: rgba(150, 150, 150, .5);
         }
       }
     }
     li>ol{
-      border:1px solid #fff;
-      border-style:inset;
+      border:1px solid #00000044;
+      box-shadow: 1px 1px 8px #000000 outset;
+      border-radius: 4px;
+      padding: 4px;
+    }
+    &>li{
+      padding:0 8px 0 8px;
+    }
+  }
+  .dark .menu{
+    background-color:transparent;
+    &>li:hover{
+      background-color: rgba(84, 84, 84, .5);
+      li{
+        &:hover{
+          background-color: rgb(62, 110, 197);
+        }
+        &:has(ol:hover){
+          background-color: rgba(84, 84, 84, .5);
+        }
+      }
+    }
+    li>ol{
+      border:1px solid #ffffff44;
+      box-shadow: 1px 1px 8px #000000 outset;
       border-radius: 4px;
       padding: 4px;
     }
--- src/myComponents/menu/menu.vue
+++ src/myComponents/menu/menu.vue
@@ -1,13 +1,13 @@
 <template>
   <li v-for="(v,k) in item?.children" @click.native="e=>v.onClick&&v.onClick(e,item,k)" :key="k">
     <el-icon v-if="item?.left" style="overflow: hidden;">
-      <img v-if="v.leftImgSrc" :src="v.leftImgSrc" class="w-full h-full" style="filter:drop-shadow(lightgrey 0 60px);transform: translateY(-60px);">
+      <img v-if="v.leftImgSrc" :src="v.leftImgSrc" class="leftImg w-full h-full">
     </el-icon>
     <span style="flex-grow: 1;display:flex;justify-content: left;">{{ v.name }}</span>
     <el-icon v-if="item?.right" style="overflow: hidden;">
-      <img v-if="v.children" src="/src/assets/caretRight.svg" class="w-full h-full" style="filter:drop-shadow(lightgrey 0 60px);transform: translateY(-60px);">
+      <img v-if="v.children" src="/src/assets/caretRight.svg" class="rightImg w-full h-full">
     </el-icon>
-    <ol class="absolute flex-col hidden p-0 left-100% top-0" v-if="v.children"><!--二、三、...级菜单-->
+    <ol class="absolute flex-col hidden p-0 left-100% top--5px" v-if="v.children"><!--二、三、...级菜单-->
       <submenu :item.sync="v"></submenu>
     </ol>
   </li>
@@ -24,3 +24,13 @@
     name: 'submenu'
   }
 </script>
+<style>
+  .leftImg{
+    filter:drop-shadow(var(--ep-text-color-primary) 0 60px);
+    transform: translateY(-60px);
+  }
+  .rightImg{
+    filter:drop-shadow(var(--ep-text-color-primary) 0 60px);
+    transform: translateY(-60px);
+  }
+</style>
