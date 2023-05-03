56dd0298f322f01d1038bc3515e5bd3f413a058c
伯虎 <2413081441@qq.com>
2023-05-03 16:29:08 +0800

    优化代码格式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import container from '../container.vue'
 // import qqLogin from '../qqlogin.vue'
-export const constantRoutes = [{
-  path: '/',
-  component: container
-},{
-  path: '/qqlogin',
-  component: () => import('../qqlogin.vue') //最好是异步加载
-}]
+export const constantRoutes = [
+  {
+    path: '/',
+    component: container
+  },
+  {
+    path: '/qqlogin',
+    component: () => import('../qqlogin.vue') //最好是异步加载
+  }
+]
 const router = createRouter({
   history: createWebHistory(),
   scrollBehavior: () => ({ top: 0 }),
