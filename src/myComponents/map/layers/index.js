1e27fbf24caf87b924ce65153db1b4fcdb2afb67
skytangleisky <2413081441@qq.com>
2023-06-08 17:21:50 +0800

    行政区划用线程池绘制

--- src/myComponents/map/layers/index.js
+++ src/myComponents/map/layers/index.js
@@ -1,6 +1,5 @@
 import MapLayer from './mapLayer.js'
 import RouteLayer from './routeLayer.js'
-import BorderLayer from './borderLayer.js'
 import IsolineLayer from './isolineLayer.js'
 import RadarLayer from './radarLayer.js'
 import PointLayer from './pointLayer.js'
@@ -9,7 +8,6 @@ import StationLayer from './stationLayer.ts'
 export {
   MapLayer,
   RouteLayer,
-  BorderLayer,
   IsolineLayer,
   RadarLayer,
   PointLayer,
