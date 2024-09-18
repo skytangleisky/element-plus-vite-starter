0b6f6d61757c85ed8c83ddfb279a7be7a668bf61
伯虎 <2413081441@qq.com>
2024-09-18 13:30:59 +0800

    修复重庆组网设备管理

--- src/myComponents/风雷达组网/pages/设备管理.vue
+++ src/myComponents/风雷达组网/pages/设备管理.vue
@@ -1,10 +1,82 @@
 <template>
   <datatable
     style="padding: 10px"
-    database="host=tanglei.top&port=3308&user=root&password=mysql&database=ryplat_bjry"
-    table="units"
+    :database="databaseRaw"
+    table="device"
+    :headerOption="headerOption"
   ></datatable>
 </template>
 <script lang="ts" setup>
+import { databaseRaw } from "~/api/重庆";
+import { reactive } from "vue";
 import datatable from "~/myComponents/datatable/index.vue";
+const headerOption = reactive({
+  id: {
+    label: "主键",
+    hide: true,
+  },
+  uuid: {
+    label: "唯一标识",
+  },
+  no: {
+    label: "编号",
+  },
+  device_name: {
+    label: "设备名称",
+  },
+  device_short_name: {
+    label: "设备简称",
+  },
+  device_type: {
+    label: "设备类型",
+  },
+  device_no: {
+    label: "设备编号",
+  },
+  device_model: {
+    label: "设备型号",
+  },
+  address: {
+    label: "地址",
+  },
+  manufacturer: {
+    label: "厂商",
+  },
+  lng: {
+    label: "经度",
+  },
+  lat: {
+    label: "纬度",
+  },
+  altitude: {
+    label: "海拔高度",
+  },
+  height: {
+    label: "气压海拔高度",
+  },
+  data_path: {
+    label: "数据路径",
+  },
+  color: {
+    label: "颜色",
+  },
+  status: {
+    label: "设备状态",
+  },
+  speed: {
+    label: "风速",
+  },
+  hide: {
+    label: "是否隐藏",
+  },
+  orientation: {
+    label: "风向",
+  },
+  cratetime: {
+    label: "创建时间",
+  },
+  updatetime: {
+    label: "更新时间",
+  },
+});
 </script>
