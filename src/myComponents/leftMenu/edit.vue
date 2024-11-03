d8a7495195302882824489a80e9f3481c4c04af6
伯虎 <2413081441@qq.com>
2024-11-03 13:41:59 +0800

    解决菜单管理和权限管理界面样式问题

--- src/myComponents/leftMenu/edit.vue
+++ src/myComponents/leftMenu/edit.vue
@@ -99,119 +99,50 @@ onBeforeUnmount(()=>{
 })
 </script>
 <style lang="scss">
-
-.dark .dd {
-  .dd-handle {
-    color: #fff;
-    border: 1px solid #999;
-    background: #bbb;
-    background: -webkit-linear-gradient(top, #bbb 0%, #999 100%);
-    background: -moz-linear-gradient(top, #bbb 0%, #999 100%);
-    background: linear-gradient(top, #bbb 0%, #999 100%);
-  }
-  .dd-handle:hover {
-    background: #bbb;
-  }
-  .dd-item > button:before {
-    color: #fff;
-  }
-}
-
-
-.dd-hover > .dd-handle {
-  background: #2ea8e5 !important;
-}
-
-/**
-        * Nestable Draggable Handles
-        */
-
-.dd3-content {
-  display: block;
-  height: 30px;
-  margin: 5px 0;
-  padding: 5px 10px 5px 40px;
-  color: #333;
-  text-decoration: none;
-  font-weight: bold;
-  border: 1px solid #ccc;
-  background: #fafafa;
-  background: -webkit-linear-gradient(top, #fafafa 0%, #eee 100%);
-  background: -moz-linear-gradient(top, #fafafa 0%, #eee 100%);
-  background: linear-gradient(top, #fafafa 0%, #eee 100%);
-  -webkit-border-radius: 3px;
-  border-radius: 3px;
-  box-sizing: border-box;
-  -moz-box-sizing: border-box;
-}
-
-.dd3-content:hover {
-  color: #2ea8e5;
-  background: #fff;
-}
-
-.dd-dragel > .dd3-item > .dd3-content {
-  margin: 0;
-}
-
-.dd3-item > button {
-  margin-left: 30px;
-}
-
-.dd3-handle {
-  position: absolute;
-  margin: 0;
-  left: 0;
-  top: 0;
-  cursor: pointer;
-  width: 30px;
-  text-indent: 30px;
-  white-space: nowrap;
-  overflow: hidden;
-  border: 1px solid #aaa;
-  background: #ddd;
-  background: -webkit-linear-gradient(top, #ddd 0%, #bbb 100%);
-  background: -moz-linear-gradient(top, #ddd 0%, #bbb 100%);
-  background: linear-gradient(top, #ddd 0%, #bbb 100%);
-  border-top-right-radius: 0;
-  border-bottom-right-radius: 0;
-}
-
-.dd3-handle:before {
-  content: "≡";
-  display: block;
-  position: absolute;
-  left: 0;
-  top: 3px;
-  width: 100%;
-  text-align: center;
-  text-indent: 0;
-  color: #fff;
-  font-size: 20px;
-  font-weight: normal;
-}
-
-.dd3-handle:hover {
-  background: #ddd;
-}
-
-
-
-
-.dd,.dd-dragel {
+.dd {
   position: relative;
+  display: block;
+  margin: 0;
+  padding: 0;
   list-style: none;
+  font-size: 13px;
+  line-height: 20px;
+  .dd-handle {
+    display: block;
+    height: 30px;
+    // margin: 5px 0;
+    padding: 5px 10px;
+    color: #333;
+    text-decoration: none;
+    font-weight: bold;
+    border: 1px solid #ccc;
+    background: #fafafa;
+    // border-radius: 3px;
+    box-sizing: border-box;
 
-  // ul {
-  //   margin-left: 20px;
-  //   padding-left: 20px;
-  //   position: relative;
-  // }
+    &:hover {
+      color: #2ea8e5;
+      background: #fff;
+    }
+  }
+  .cross{
+    cursor: pointer;
+    z-index: 1;
+    #morph {
+      d: path("M0,75 L200,75 200,125 0,125z");
+    }
+  }
+  .dd-collapsed>.cross{
+    #morph {
+      d: path("M75,0 L125,0 125,75 200,75 200,125 125,125 125,200 75,200 75,125 0,125 0,75 75,75z");
+    }
+  }
 
   li {
     position: relative;
     padding-left: 30px;
-
+    box-sizing: border-box;
+    width: 100%;
     &::before,&::after {
       content: '';
       position: absolute;
@@ -238,6 +169,19 @@ onBeforeUnmount(()=>{
 
     &:last-child::before {
       height: 15px;
+    }
+  }
+}
+.dark .dd {
+  .dd-handle {
+    color: #fff;
+    border: 1px solid #999;
+    background: #bbb;
+    background: -webkit-linear-gradient(top, #bbb 0%, #999 100%);
+    background: -moz-linear-gradient(top, #bbb 0%, #999 100%);
+    background: linear-gradient(top, #bbb 0%, #999 100%);
+    &:hover {
+      background: #bbb;
     }
   }
 }
