ad8dcbf4bfae6256e44b949b5d0640ee469dfa37
唐雷 <2413081441@qq.com>
2025-03-21 15:03:43 +0800

    飞机航迹不再从陈俊驰那里获取

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,12 @@
   </el-config-provider>
   <websocket v-if="checkPermission(['admin', 'zh'])"></websocket>
   <debug v-if="checkPermission(['admin', 'zh'])"></debug>
-  <AircraftPos></AircraftPos>
 </template>
 <script lang="ts" setup>
 import { onMounted,nextTick, onBeforeUnmount } from "vue";
 import { checkPermission } from "./tools";
 import websocket from "~/myComponents/websocket/index.vue";
 import debug from "~/myComponents/websocket/debug.vue";
-import AircraftPos from "~/myComponents/websocket/aircraftPosition.vue";
 import { useUserStore } from "./stores/user";
 const user = useUserStore();
 import { useExclude } from "./myComponents/bus";
