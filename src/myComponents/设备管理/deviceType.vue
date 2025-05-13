3788a543317ae37df9df91693b3b5fd8a71294c3
唐雷 <2413081441@qq.com>
2025-05-13 13:15:20 +0800

    加入设备管理相关界面

--- src/myComponents/设备管理/deviceType.vue
+++ src/myComponents/设备管理/deviceType.vue
@@ -1,13 +1,33 @@
 <template>
-  <div class="main-container">
-    <h1>设备类型</h1>
-  </div>
+  <datatable
+    style="padding: 10px"
+    :database="databaseRaw2"
+    table="device_type"
+    :headerOption="headerOption"
+  ></datatable>
 </template>
-<script lang="ts" setup></script>
-<style lang="scss" scoped>
-.main-container {
-  background:white;
-  position: absolute;
-  inset: 0;
-}
-</style>
+<script lang="ts" setup>
+import { databaseRaw2 } from "~/api/重庆";
+import { reactive } from "vue";
+import datatable from "~/myComponents/datatable/index.vue";
+const headerOption = reactive({
+  // id: { label: "主键" },
+  // createtime: { label: "添加时间" },
+  // updatetime: { label: "更新时间" },
+  // uuid: { label: "唯一标识" },
+  // username: { label: "用户名" },
+  // introduction: { label: "描述" },
+  // nickname: { label: "昵称" },
+  // login_type: { label: "登录方式" },
+  // email: { label: "电子邮箱" },
+  // email_verified_at: { label: "电子邮箱确认时间" },
+  // password: { label: "密码" },
+  // two_factor_secret: { label: "谷歌双因子认证秘钥" },
+  // two_factor_recovery_codes: { label: "谷歌双因子认证恢复码" },
+  // remember_token: { label: "记录token" },
+  // avatar: { label: "头像地址" },
+  // user_path: { label: "相对路径" },
+  // debug_enable: { label: "是否允许调试" },
+  // roles: { label: "角色" },
+});
+</script>
